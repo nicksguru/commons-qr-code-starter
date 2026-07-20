29dc76782bbc58ca588cdee74449e73e04e8ac79
Nick S. <job-offers@nicks.guru>
2026-07-20 10:59:09 +0300

    build: bump version number

--- src/main/java/guru/nicks/commons/qrcode/utils/QrCodeUtils.java
+++ src/main/java/guru/nicks/commons/qrcode/utils/QrCodeUtils.java
@@ -1,11 +1,11 @@
 package guru.nicks.commons.qrcode.utils;
 
+import com.github.auties00.qr.QrTerminal;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import it.auties.qr.QrTerminal;
 import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
